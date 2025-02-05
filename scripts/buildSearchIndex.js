f0db823ca1e79f5146a13a1c5965ead7ce27be87
Oz <ozcodx@gmail.com>
2025-02-04 23:17:18 -0500

    Add mob database processing with web sprite scraping support

--- scripts/buildSearchIndex.js
+++ scripts/buildSearchIndex.js
@@ -2,6 +2,8 @@ const { subscribe } = require('diagnostics_channel');
 const fs = require('fs');
 const lunr = require('lunr');
 const path = require('path');
+const cheerio = require('cheerio');
+const axios = require('axios');
 
 // Funciones para preparar los datos
 function cleanDescription(description) {
@@ -41,6 +43,7 @@ function cleanName(name) {
 
 function cleanCondensed(condensedValue) {
     // extraer los id de categoria (cada potencia de 2 es una categoria)
+
     const categories = [];
     for (let i = 0; i < 32; i++) {
         if (condensedValue & (1 << i)) {
@@ -53,9 +56,14 @@ function cleanCondensed(condensedValue) {
 // funciones para procesar los datos
 
 async function processNames(namesFile) {
+    if (!fs.existsSync(namesFile)) {
+        console.error('Error: El archivo de nombres no existe');
+        return new Map();
+    }
 
     const names = new Map();
     const nameLines = fs.readFileSync(namesFile, 'utf-8').split('\n');
+
 
     
     for (const line of nameLines) {
@@ -70,9 +78,15 @@ async function processNames(namesFile) {
 }
 
 async function processDescriptions(descFile) {
+    if (!fs.existsSync(descFile)) {
+        console.error('Error: El archivo de descripciones no existe');
+        return new Map();
+    }
+
     const descriptions = new Map();
     let currentId = null;
     let currentDescription = [];
+
 
     // Leer archivo de descripciones
     const descLines = fs.readFileSync(descFile, 'utf-8').split('\n');
@@ -110,7 +124,7 @@ async function processDescriptions(descFile) {
     return descriptions;
 }
 
-async function processIndex(documents) {
+async function processItemIndex(documents) {
      // Construir el índice
      const idx = lunr(function () {
         this.ref('id');
@@ -125,12 +139,33 @@ async function processIndex(documents) {
     return idx;
 }
 
-async function processDb(dbFile) {
+async function processMobIndex(documents) {
+    const idx = lunr(function () {
+        this.ref('id');
+        this.field('name', { boost: 10 }); // Dar más peso al nombre
+        this.field('name2');
+
+        // Agregar cada documento al índice
+        documents.forEach(function (doc) {
+            this.add(doc);
+        }, this);
+    });
+    return idx;
+}
+
+async function processItemDb(dbFile) {
     const items = new Map();
+
     const types = new Map();
+
+    if (!fs.existsSync(dbFile)) {
+        console.error('Error: El archivo de base de datos no existe');
+        return { items, types };
+    }
     
     try {
         // read the db file
+
         const rawData = fs.readFileSync(dbFile, 'utf-8');
         const db = JSON.parse(rawData);
 
@@ -184,7 +219,96 @@ async function processDb(dbFile) {
     }
 }
 
-async function processImages(folder) {
+async function processMobDb(dbFile) {
+    const mobs = new Map();
+
+    console.log(`Procesando mobs...`);
+
+    if (!fs.existsSync(dbFile)) {
+        console.error('Error: El archivo de base de datos no existe');
+        return { mobs };
+    }
+
+    try {
+        // read the db file
+        console.log(`Leyendo archivo de base de datos...`);
+        const rawData = fs.readFileSync(dbFile, 'utf-8');
+        const db = JSON.parse(rawData);
+
+        console.log(`Procesando ${db.length} mobs...`);
+
+        // for each mob in db, take the id as the key and format the mob data.
+
+        for (const mob of db) {
+            const id = mob.ID.toString();
+            const mobData = {
+                name: mob.iName,
+                name2: mob.kName,
+                sprite: mob.Sprite,
+                hp: mob.HP,
+                lvl: mob.LV,
+                def: mob.DEF,
+                mdef: mob.MDEF,
+                atk: Math.min(mob.ATK1, mob.ATK2) + " - " + Math.max(mob.ATK1, mob.ATK2),
+
+                //stats
+                str: mob.STR,
+                agi: mob.AGI,
+                vit: mob.VIT,
+                int: mob.INT,
+                dex: mob.DEX,
+                luk: mob.LUK,
+                //experience
+                exp: mob.EXP,
+                jexp: mob.JEXP,
+                mexp: mob.MEXP, //mvp experience
+                // characteristics
+                element: mob.Element,
+                size: mob.Scale,
+                race: mob.Race,
+            }
+            // add the mob drop
+            const dropTable = [];
+            for (let i = 0; i < 10; i++) {
+                if (mob["Drop" + i + "id"] !== 0 && mob["Drop" + i + "per"] !== 0) {
+                    dropTable.push({
+                        type: "normal",
+                        id: mob["Drop" + i + "id"],
+                        per: mob["Drop" + i + "per"]
+                    });
+                }
+            }
+            // check the card drop
+            if (mob["DropCardid"] !== 0 && mob["DropCardper"] !== 0) {
+                dropTable.push({
+                    type: "card",
+                    id: mob["DropCardid"],
+                    per: mob["DropCardper"]
+                });
+            }
+            // check the mvp drop
+            for (let i = 1; i <= 3; i++) {
+                if (mob["MVPNid" + i] !== 0 && mob["MVPNper" + i] !== 0) {
+                    dropTable.push({
+                        type: "mvp",
+                        id: mob["MVPNid" + i],
+                        per: mob["MVPNper" + i]
+                    });
+                }
+            }
+            // add the drop table to the mob data
+            mobData.drop = dropTable;
+
+            mobs.set(id, mobData);
+        }
+        return mobs;
+    } catch (error) {
+        console.error('Error procesando el archivo de base de datos:', error);
+        return { mobs: new Map() };
+    }
+}
+
+async function processItemImages(folder) {
     if (!fs.existsSync(folder)) {
         console.log(`Carpeta ${folder} no encontrada, saltando procesamiento de imágenes`);
         return { batches: [], idMap: new Map() };
@@ -237,6 +361,117 @@ async function processImages(folder) {
     }
 }
 
+// mini scrapper tool
+// using https://nn.ai4rei.net/dev/npclist/?q={sprite}&qq=search&s=kro as alternative source
+
+
+async function searchSpriteWeb(sprite, folder) {
+    console.log(`Buscando sprite ${sprite} en la web...`);
+    // Pequeño timeout para evitar limitaciones de tasa
+    await new Promise(resolve => setTimeout(resolve, 300));
+    const baseUrl = 'https://nn.ai4rei.net/dev/npclist/';
+    const url = `${baseUrl}?q=${sprite}&qq=search&s=kro`;
+    
+    try {
+        const response = await axios.get(url, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+            }
+        });
+
+        const $ = cheerio.load(response.data);
+        const contentMatrix = $('#content-matrix');
+        const firstDiv = contentMatrix.children().first();
+        const img = firstDiv.find('img');
+        const src = img.attr('src');
+
+        // remove "i/" at beginning of the string
+        // and remove .gif at the end of the string
+        const imageUrl = src.replace('i/', '').replace('.gif', '');
+        console.log(`Encontrado sprite ${sprite} en la web: ${imageUrl}`);
+
+        return imageUrl;
+    } catch (error) {
+        console.error(`Error al buscar o descargar el sprite: ${error.message}`);
+        return undefined;
+    }
+
+}
+
+
+
+async function processMobImages(database, folder) {
+    if (!fs.existsSync(folder)) {
+        console.log(`Carpeta ${folder} no encontrada, saltando procesamiento de imágenes`);
+        return { batches: [], idMap: new Map() };
+    }
+    
+    const batchSize = 1000;
+    const batches = [];
+    let currentBatch = new Map();
+    const idToBatch = new Map();
+    const missingSprites = [];
+
+    try {
+        const imageFiles = fs.readdirSync(folder);
+        console.log(`Procesando ${imageFiles.length} imágenes de ${folder}`);
+
+        //loop through the database and get the id and the sprite name
+        for (const [id, mob] of database) {
+            const sprite = mob.sprite;
+
+            // read the file using the sprite as name and gif as extension
+            let file;
+            try {
+                file = fs.readFileSync(path.join(folder, sprite + '.gif'));
+            } catch (error) {
+                console.log(`Sprite ${sprite} no encontrado, buscando en la web...`);
+                const result = await searchSpriteWeb(sprite, folder);
+                if (result) {
+                    try { 
+
+                        file = fs.readFileSync(path.join(folder, result + '.gif'));
+                    } catch (error) {
+                        // save the sprite name
+                        missingSprites.push(sprite);
+                        continue;
+                    }
+                } else {
+                    // save the sprite name
+                    missingSprites.push(sprite);
+                    continue;
+                }
+            }
+
+            const base64 = file.toString('base64');
+            const urlData = `data:image/gif;base64,${base64}`;
+
+            if (currentBatch.size >= batchSize) {
+                batches.push(currentBatch);
+                currentBatch = new Map();
+            }
+
+            // Agregar el sprite al batch actual
+            currentBatch.set(id, urlData);
+            idToBatch.set(id, batches.length);
+        }
+
+        // Agregar el último batch si contiene imágenes
+        if (currentBatch.size > 0) {
+            batches.push(currentBatch);
+        }
+        
+        // save the missing sprites
+        fs.writeFileSync(path.join(folder, 'missing_sprites.txt'), missingSprites.join('\n'));
+        console.log(`Mobs con sprites faltantes: ${missingSprites.length}`);
+
+        return { batches, idMap: idToBatch };
+
+    } catch (error) {
+        console.error(`Error procesando imágenes en ${folder}:`, error);
+        return { batches: [], idMap: new Map() };
+    }
+}
 // funcion para guardar archivos json
 function writeJsonFile(filename, data) {
     // crear carpeta de salida si no existe
@@ -260,16 +495,59 @@ function writeJsonFile(filename, data) {
     );
 }
 
+async function processMobFiles(files) {
+    console.log('Procesando archivos de mobs...');
+    const outputPath = './data';
+
+    try {
+        // 1. procesar base de datos
+        console.log('Procesando base de datos...');
+        const mobs = await processMobDb(files.mob_dbFile);
+
+        // 2. construir indice de búsqueda
+        console.log('Construyendo índice de búsqueda...');
+        const idx = await processMobIndex(mobs);
+
+        // 3. procesar imágenes
+        console.log('Procesando imágenes...');
+        const { batches: mobBatches, idMap: mobIdMap } = await processMobImages(mobs, files.mob_spritesFolder);
+
+        // crear descriptor de imágenes
+        const imageDescriptor = Object.fromEntries(mobIdMap);
+
+        // guardar batches de imágenes 
+        console.log('Guardando batches de imágenes...');
+        for (let i = 0; i < mobBatches.length; i++) {
+            writeJsonFile(`mob_sprites_batch_${i}.json`, mobBatches[i]);
+            console.log(`Batch de imágenes ${i} guardado (${mobBatches[i].size} imágenes)`);
+        }
+
+
+        // 5. guardar archivos
+        console.log('Guardando archivos...');
+        writeJsonFile('mobs.json', mobs);
+        writeJsonFile('mob-search-index.json', idx);
+        writeJsonFile('mob-images-descriptor.json', imageDescriptor);
+
+        console.log('\nProcesamiento completado con éxito!');
+
+    } catch (error) {
+        console.error('Error en el procesamiento:', error);
+    }
+}
+
+
 // funcion para procesar los archivos
-async function processFiles(files) {
+async function processItemFiles(files) {
     const outputPath = './data';
 
     try {
         // 1. procesar los archivos de nombres y descripciones
         console.log('Procesando nombres...');
-        const names = await processNames(files.namesFile);
+        const names = await processNames(files.item_namesFile);
         console.log('Procesando descripciones...');
-        const descriptions = await processDescriptions(files.descFile);
+        const descriptions = await processDescriptions(files.item_descFile);
+
 
         const documents = [];
 
@@ -288,7 +566,7 @@ async function processFiles(files) {
 
         // construir el indice
         console.log('Construyendo índice de búsqueda...');
-        const idx = await processIndex(documents);
+        const idx = await processItemIndex(documents);
 
         // remove searchdesc from documents
         documents.forEach(doc => {
@@ -297,18 +575,21 @@ async function processFiles(files) {
 
         // write json files
         console.log('Guardando archivos...');
-        writeJsonFile('search-index.json', idx);
-        writeJsonFile('namedesc.json', documents);
+        writeJsonFile('item-search-index.json', idx);
+        writeJsonFile('item-namedesc.json', documents);
 
-        console.log(`Índice de búsqueda creado en ${outputPath}/search-index.json`);
-        console.log(`Documentos guardados en ${outputPath}/namedesc.json`);
+
+        console.log(`Índice de búsqueda creado en ${outputPath}/item-search-index.json`);
+        console.log(`Documentos guardados en ${outputPath}/item-namedesc.json`);
+
 
         // 2. Procesar los archivos de base de datos
         console.log('Procesando base de datos...');
-        const { items, types } = await processDb(files.dbFile);
+        const { items, types } = await processItemDb(files.item_dbFile);
         
+
         writeJsonFile('items.json', items);
-        writeJsonFile('types.json', types);
+        writeJsonFile('item-types.json', types);
         
         console.log(`Base de datos guardada en ${outputPath}/items.json`);
         console.log(`Tipos guardados en ${outputPath}/types.json`);
@@ -317,10 +598,12 @@ async function processFiles(files) {
         console.log('\nProcesando imágenes...');
         
         console.log('Procesando iconos...');
-        const { batches: iconBatches, idMap: iconIdMap } = await processImages(files.iconsFolder);
+        const { batches: iconBatches, idMap: iconIdMap } = await processItemImages(files.item_iconsFolder);
         
+
         console.log('Procesando ilustraciones...');
-        const { batches: illustrationBatches, idMap: illustrationIdMap } = await processImages(files.illustrationsFolder);
+        const { batches: illustrationBatches, idMap: illustrationIdMap } = await processItemImages(files.item_illustrationsFolder);
+
 
         // Crear el descriptor de imágenes
         const imageDescriptor = {
@@ -352,14 +635,24 @@ async function processFiles(files) {
     }
 }
 
+async function processFiles(files) {
+    //await processItemFiles(files);
+    await processMobFiles(files);
+}
+
 // Ejecutar el proceso
 processFiles({
-    namesFile: './data/input/idnum2itemdisplaynametable.txt',
-    descFile: './data/input/idnum2itemdesctable.txt',
-    dbFile: './data/input/item_db.json',
-    iconsFolder: './data/input/icon',
-    illustrationsFolder: './data/input/illustration'
+    // item files
+    item_namesFile: './data/input/idnum2itemdisplaynametable.txt',
+    item_descFile: './data/input/idnum2itemdesctable.txt',
+    item_dbFile: './data/input/item_db.json',
+    item_iconsFolder: './data/input/icon',
+    item_illustrationsFolder: './data/input/illustration',
+    // mobs files
+    mob_dbFile: './data/input/mob_db.json',
+    mob_spritesFolder: './data/input/sprites'
 }).catch(console.error); 
+
 
 /* data structures:
 
@@ -432,6 +725,58 @@ illustrations_batch_0.json:
 {
     "1": "urldata", // the key is the id, the value is the urldata
     "2": "urldata"
+}
+
+mob-search-index.json:
+{
+    "index": {
+        "fields": ["name", "name2"]
+    }
+}   
+
+mob-images-descriptor.json:
+{
+    "1": 0, // the key is the id, the value is the batch number
+    "2": 0
+}
+
+
+mob_sprites_batch_0.json:
+{
+    "1": "urldata", // the key is the id, the value is the urldata
+    "2": "urldata"
+}   
+
+mobs.json:
+{
+    "id": "1",
+    "name": "mob name",
+    "name2": "mob name",
+    "sprite": "mob sprite",
+    "hp": 1,
+    "lvl": 1,
+    "def": 1,
+    "mdef": 1,
+    "atk": 1,
+    "str": 1,
+    "agi": 1,
+    "vit": 1,
+    "int": 1,
+    "dex": 1,
+    "luk": 1,
+    "exp": 1,
+    "jexp": 1,
+    "mexp": 1,
+    "element": 1,
+    "size": 1,
+    "race": 1,
+    "drop": [
+        {
+            "id": 1,
+            "type": "can be normal, card or mvp",
+            "per": 1,
+        }
+    ]
 }
 
 
